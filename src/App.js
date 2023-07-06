5c2c2f43a946af8a4aa5c2fe6398ca024bbb4415
Abhishek Poddar <67195536+abhi8054@users.noreply.github.com>
2023-07-07 00:47:17 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='wrapper'>
+        <div className='header__top'>
+          <div className='the_100_exch'>
+            <img src='/img/the100exch.png' alt='' />
+          </div>
+          <div className='win'>
+            <h2>WIN EVERYWHERE</h2>
+            <h2>#LAGISHART</h2>
+          </div>
+        </div>
+        <div className='lucky__theme'>
+          <div className='z_index'>
+            <p>PLAY ON YOUR LUCKY THEME</p>
+          </div>
+        </div>
+        <div className='grid'>
+          <div className='child'>
+          <a href='http://54.88.130.203/#!/'>
+            <img src='/img/skyimg.webp' alt='' />
+            </a>
+          </div>
+          <div className='child'>
+            <a href='http://54.88.130.203:3000/Login'>
+              <img src='/img/diamondimg.webp' alt='' />
+            </a>
+          </div>
+          <div className='child'>
+              <a href='#'>
+            <img src='/img/lotusimg.webp' alt='' />
+            </a>
+          </div>
+          <div className='child'>
+              <a href='http://207.180.199.240/'>
+            <img src='/img/worldimg.webp' alt='' />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
